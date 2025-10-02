1c19a14cd6d82c0a7fd39ee1ba46039d5653eaeb
Jeem312 <shanjidajeem312@gmail.com>
2025-10-02 09:44:55 +0600

    feat(cors): add PATCH method to CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(cors({
     "http://localhost:5173",
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"]
 }));
 
